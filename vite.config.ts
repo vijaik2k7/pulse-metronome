8ac67532c73338eb150f5920b393d32433f69d0b
Vijai <vijai@Vijais-MacBook-Pro.local>
2026-09-12 00:47:57 -0700

    Update cache buster asset filenames for instant GitHub Pages update

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}[extname]`,
+      },
     },
   },
 });
